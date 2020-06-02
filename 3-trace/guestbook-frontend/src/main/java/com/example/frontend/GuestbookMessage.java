3c534df3c767da887ca5d0b352f44d2947242203
dchat <d.chathusanka@gmail.com>
2020-06-02 22:56:05 +0530

    cloud trace

--- 3-trace/guestbook-frontend/src/main/java/com/example/frontend/GuestbookMessage.java
+++ 3-trace/guestbook-frontend/src/main/java/com/example/frontend/GuestbookMessage.java
@@ -13,4 +13,43 @@ public class GuestbookMessage extends RepresentationModel<GuestbookMessage> {
 
   private String imageUri;
 
+  public Long getId()
+  {
+    return id;
+  }
+
+  public void setId( Long id )
+  {
+    this.id = id;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName( String name )
+  {
+    this.name = name;
+  }
+
+  public String getMessage()
+  {
+    return message;
+  }
+
+  public void setMessage( String message )
+  {
+    this.message = message;
+  }
+
+  public String getImageUri()
+  {
+    return imageUri;
+  }
+
+  public void setImageUri( String imageUri )
+  {
+    this.imageUri = imageUri;
+  }
 }
